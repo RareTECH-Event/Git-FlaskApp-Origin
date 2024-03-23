81e0c8c1cbb97bb07272308466e4ff49823e3c90
Rinasham <rinashamau@gmail.com>
2024-03-23 22:23:45 +1100

    [FE/BE] indexページの作成

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
