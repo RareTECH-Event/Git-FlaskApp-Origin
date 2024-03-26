77cf6ac62606d8040abc2ba978a8498a750046a3
Rinasham <rinashamau@gmail.com>
2024-03-26 12:55:41 +1100

    [BE][delete] error関数削除

--- WebApp/app.py
+++ WebApp/app.py
@@ -6,10 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/error')
-def error():
-    return render_template('error.html')
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
